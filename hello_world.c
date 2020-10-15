efa45010fdd2c6d770166e5a13fc0b4723aeb736
m0sk <m0sk.2x2@gmail.com>
2020-10-15 22:38:58 +0300

    "\n" in printf

--- hello_world.c
+++ hello_world.c
@@ -3,7 +3,7 @@ import <stdio.h>
 int main()
 {
     // Test
-    printf("Hello, world! It's me!\n");
+    printf("Hello, world!\n It's me!\n");
     char* text = "Test1";
     printf("%s\n",text);
     return 0;
