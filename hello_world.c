ad202420e8490296a396cc76e8de2b7cef382bb9
m0sk <m0sk.2x2@gmail.com>
2021-06-29 09:40:13 +0300

    Update hello_world.c
    Some changes

--- hello_world.c
+++ hello_world.c
@@ -5,6 +5,8 @@ int main()
     // Test
     printf("Hello, world!\n It's me!\n");
     char* text = "Test1";
+    char* test_str = "Test string";
     printf("%s\n",text);
+    printf("%s\n",test_str);
     return 0;
 }
